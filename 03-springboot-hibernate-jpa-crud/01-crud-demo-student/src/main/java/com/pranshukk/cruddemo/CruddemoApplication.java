80e042c61fdbf4a57658a854312b3ea65b1393aa
Pranshu <pranshukk95@gmail.com>
2025-10-08 23:46:20 +0530

    adding 03-springboot-hibernate-jpa-crud

---
+++ 03-springboot-hibernate-jpa-crud/01-crud-demo-student/src/main/java/com/pranshukk/cruddemo/CruddemoApplication.java
@@ -0,0 +1,22 @@
+package com.pranshukk.cruddemo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+	// this method will run after the spring beans have been loaded
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner -> {
+			System.out.println("Hello World");
+		};
+	}
+}
